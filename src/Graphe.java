ab6c2b231b8b400797b26bdef298dddc5cf1b2d5
Florian Mangin <manginf54@gmail.com>
2022-06-07 08:44:01 +0200

    ADD Interface Graphe

---
+++ src/Graphe.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+/**
+ * Cette classe permet de manipuler un graphe
+ */
+public interface Graphe {
+    /**
+     * Renvoie la liste des noeuds du graphe
+     * @return la liste de noeuds
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Renvoie la liste des arcs partant d'un noeud
+     * @param n Nom du noeud
+     * @return la liste des arcs partant du noeud
+     */
+    public List<Arc> suivants(String n);
+}

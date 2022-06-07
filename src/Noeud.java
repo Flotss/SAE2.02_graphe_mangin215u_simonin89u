88e459119df147f85db69f30a2cf7efb5ac3690b
Nzosim <nzosim54@gmail.com>
2022-06-07 08:32:12 +0200

    add Noeud.java

---
+++ src/Noeud.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Noeud {
+
+    private String nom;
+    private List<Arc> adj;
+
+    public Noeud(){
+        throw new Error("TODO");
+    }
+
+    public boolean equals(Object o){
+        throw new Error("TODO");
+    }
+
+    public void ajouterArc(String destination, double cout){
+        throw new Error("TODO");
+    }
+
+}

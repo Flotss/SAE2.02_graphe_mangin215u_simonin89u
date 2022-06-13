8470fd7a661800594dda729764faea95960ac831
Nzosim <nzosim54@gmail.com>
2022-06-13 11:48:06 +0200

    javadoc BellmanFord

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -11,22 +11,27 @@ public class BellmanFord {
      * @return un objet Valeur qui contient les plus courts chemins et leur cout entre chaque noeud
      */
     public static Valeur resoudre(Graphe g, String depart){
+        // initialisation de l'objet Valeur
         Valeur val = new Valeur();
+        // initialise toutes les valeurs a + l infini et leur parent a null
         for (String n : g.listeNoeuds()){
             val.setValeur(n, Double.MAX_VALUE);
             val.setParent(n, null);
         }
-
+        // initialise un objet Valeur avec les valeurs de val
         Valeur ancien = new Valeur(val);
+        // on assigne la valeur du noeud de depart a 0
         val.setValeur(depart, 0);
 
         int pointFixe = val.valeur.size();
         int i =0;
+        // tant que val et ancien ne sont pas egaux et que le nombre d iteration est inferieur a la taille du graphe on continue
         while (!val.equals(ancien) && pointFixe > i){
-            // Ancien devient val
+            // Ancien est assigne a val
             ancien = new Valeur(val);
-
+            // On parcourt tous les noeuds du graphe
             for (String n : g.listeNoeuds()){
+                // On parcourt tous les arcs de chaque noeuds
                 for (Arc a : g.suivants(n)){
                     if (val.getValeur(n) + a.getCout() < val.getValeur(a.getDest())){
                         val.setValeur(a.getDest(), val.getValeur(n) + a.getCout());
@@ -36,7 +41,7 @@ public class BellmanFord {
             }
             i++;
         }
-
+        // retour de l objet Valeur
         return val;
     }
 }

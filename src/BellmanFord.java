61f6d1a91498e137f26b65b4efe033f4500e1177
Florian Mangin <manginf54@gmail.com>
2022-06-13 14:09:39 +0200

    Français

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -13,7 +13,7 @@ public class BellmanFord {
     public static Valeur resoudre(Graphe g, String depart){
         // initialisation de l'objet Valeur
         Valeur val = new Valeur();
-        // initialise toutes les valeurs a + l infini et leur parent a null
+        // initialise toutes les valeurs a + l'infini et leur parent a null
         for (String n : g.listeNoeuds()){
             val.setValeur(n, Double.MAX_VALUE);
             val.setParent(n, null);
@@ -25,13 +25,13 @@ public class BellmanFord {
 
         int pointFixe = val.valeur.size();
         int indice =0;
-        // tant que val et ancien ne sont pas egaux et que le nombre d iteration est inferieur a la taille du graphe on continue
+        // tant que val et ancien ne sont pas égaux et que le nombre d'itération est inferieur a la taille du graphe, on continue
         while (!val.equals(ancien) && pointFixe > indice){
             // Ancien est assigne a val
             ancien = new Valeur(val);
             // On parcourt tous les noeuds du graphe
             for (String n : g.listeNoeuds()){
-                // On parcourt tous les arcs de chaque noeuds
+                // On parcourt tous les arcs de chaque nœud
                 for (Arc a : g.suivants(n)){
                     if (val.getValeur(n) + a.getCout() < val.getValeur(a.getDest())){
                         val.setValeur(a.getDest(), val.getValeur(n) + a.getCout());

6e8f4ad5f169fba90d6337eb62de118f7f180872
Nzosim <nzosim54@gmail.com>
2022-06-13 14:36:43 +0200

    djkstraopfi

---
+++ src/Dijkstra.java
@@ -0,0 +1,48 @@
+//Entrees :
+//        G un graphe oriente avec une ponderation (poids) positive des arcs
+//        A un sommet (depart) de G
+//        Debut
+//        Q <- {} // utilisation d’une liste de noeuds a traiter
+//        Pour chaque sommet v de G faire
+//          v.distance <- Infini
+//          v.parent <- Indefini
+//          Q <- Q U {v} // ajouter le sommet v `a la liste Q
+//        Fin Pour
+//        A.distance <- 0
+//        Tant que Q est un ensemble non vide faire
+    //        u <- un sommet de Q telle que u.distance est minimale
+    //        Q <- Q \ {u} // enlever le sommet u de la liste Q
+    //        Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+        //        D <- u.distance + poids(u,v)
+        //        Si D < v.distance
+            //        Alors v.distance <- D
+            //        v.parent <- u
+        //        Fin Si
+    //        Fin Pour
+//        Fin Tant que
+//        Fin
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dijkstra {
+    public static Valeur resoudre(Graphe g, String depart){
+        List<Noeud> Q = new ArrayList<>();
+        Valeur val = new Valeur();
+        for (String v : g.listeNoeuds()){
+            val.setValeur(v, Double.MAX_VALUE);
+            val.setParent(v, null);
+            Q.add(new Noeud(v));
+        }
+        val.setValeur(depart, 0);
+        while(!Q.isEmpty()){
+
+        }
+
+
+
+        return new Valeur();
+    }
+
+}
+

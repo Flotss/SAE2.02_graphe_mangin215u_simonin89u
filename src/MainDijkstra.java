01163ebe179382cd31d7eb91a76a659de722a197
Florian Mangin <manginf54@gmail.com>
2022-06-13 15:11:50 +0200

    WIP MAIN

--- src/MainDijkstra.java
+++ src/MainDijkstra.java
@@ -1,3 +1,13 @@
-public class MainDijkstra {
+import java.io.IOException;
 
+public class MainDijkstra {
+    public static void main(String[] args) throws IOException {
+        // lecture des graphes `a partir de fichiers texte
+        GrapheListe graphe = new GrapheListe("graphe_exemple1.txt");
+
+        // affichage du graphe
+        System.out.println(graphe);
+
+
+    }
 }

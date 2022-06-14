66bd201aa93af8f4928462b56f4f135e3411a58d
Nzosim <nzosim54@gmail.com>
2022-06-14 15:03:32 +0200

    javadoc main

--- src/Main/MainComparaisonMoyenne.java
+++ src/Main/MainComparaisonMoyenne.java
@@ -7,14 +7,20 @@ import Classe.GrapheListe;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * classe qui permet de comparer le moyenne de temps par graphe pour les deux methodes
+ */
 public class MainComparaisonMoyenne {
     public static void main(String[] args) throws IOException {
-        // Faire une moyenne de temps de tout les graphes
+        // initialisation des moyennes
         long moyenneTempsDjikstra = 0;
         long moyenneTempsBellman = 0;
 
+        // initialisation du dossier et du nombre de fichier
         File folder = new File("graphesGenere");
         int nombreDeGraphe = folder.listFiles().length;
+
+        // on parcourt le dossier et on calcule le chemins minimal pour chaque graphe avecc la methode de Djikstra
         int i = 0;
         for (File file : folder.listFiles()) {
             GrapheListe graphe = new GrapheListe(file.getPath());
@@ -27,6 +33,7 @@ public class MainComparaisonMoyenne {
         }
         moyenneTempsDjikstra /= nombreDeGraphe;
 
+        // on parcourt le dossier et on calcule le chemins minimal pour chaque graphe avecc la methode de BellmanFord
         i = 0;
         for (File file : folder.listFiles()) {
             GrapheListe graphe = new GrapheListe(file.getPath());
@@ -39,6 +46,7 @@ public class MainComparaisonMoyenne {
         }
         moyenneTempsBellman /= nombreDeGraphe;
 
+        // on affiche les resultats
         System.out.println("Temps moyen de Dijkstra : " + moyenneTempsDjikstra);
         System.out.println("Temps moyen de Bellman : " + moyenneTempsBellman);
 
